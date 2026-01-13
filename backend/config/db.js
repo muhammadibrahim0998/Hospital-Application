fa52507c3e3740f40fbeaa2e8a4556f4d4faf514
Hasnain-Ahmad48 <hasnaink1480@gmail.com>
2026-01-13 10:14:29 +0500

    fixed conection issue

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ import mysql from "mysql2";
 const db = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "rootuser",
+  password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME || "hospital_system",
 });
 
